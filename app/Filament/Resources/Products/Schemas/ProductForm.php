d2b08934f6d2b8301c7048574e0ae621484833b4
wawanher487 <wawanher487@gmail.com>
2026-06-06 13:21:43 +0700

    Add Product management functionality with Create, Edit, List pages and associated resources

---
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class ProductForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('$'),
+                DateTimePicker::make('createdAt')
+                    ->required(),
+                DateTimePicker::make('updatedAt')
+                    ->required(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Resources\Products\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ProductsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->label('ID'),
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('price')
+                    ->money()
+                    ->sortable(),
+                TextColumn::make('createdAt')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('updatedAt')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
